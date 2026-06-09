f1b52d7c8d0ad0594ee816ad01fb7117f860bb9e
TijanaLero <tijana.lero@gmail.com>
2026-06-09 18:00:56 +0200

    set up expo router folder structure and initialize firebase auth

---
+++ app/(app)/admin/index.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function AdminHome() {
+  return (
+    <View>
+      <Text>Admin Home</Text>
+    </View>
+  );
+}
---
+++ app/(app)/admin/kozmeticar/index.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function KozmeticarHome() {
+  return (
+    <View>
+      <Text>Kozmetičar Home</Text>
+    </View>
+  );
+}
